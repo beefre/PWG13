240eefbf2e7aace98480fd9bf5af601323ad7d83
edgarin6367 <85706605+edgarin6367@users.noreply.github.com>
2023-03-09 16:51:28 -0600

    Add files via upload

---
+++ PW3/src/main/java/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public int suma(int a, int b){
+        return a + b;
+    }
+    public int resta(int a, int b){
+        return a - b;
+    }
+    public int muiltiplo(int a, int b) {
+        return a * b;
+    }
+}
+
