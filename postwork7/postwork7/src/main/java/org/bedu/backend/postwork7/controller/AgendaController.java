d91c567dd6d349e9c41179d7567c1eebbe84f4dd
beefre <omarvalentin@gmail.com>
2023-03-25 13:35:35 -0600

    Corregido parametro de errores para desplegar mensajes de validacion

--- postwork7/postwork7/src/main/java/org/bedu/backend/postwork7/controller/AgendaController.java
+++ postwork7/postwork7/src/main/java/org/bedu/backend/postwork7/controller/AgendaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,7 +37,7 @@ public class AgendaController {
     }
 
     @PostMapping("/registro")
-    public ModelAndView registra(@Valid Persona persona) {
+    public ModelAndView registra(@Valid Persona persona, Errors errors) {
 
         agendaService.guardarPersona(persona);
 
